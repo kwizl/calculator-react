84d87ad0435270d055a01da351166f3d70fa46b7
kwizl <karis.njoroge@gmail.com>
2021-01-19 02:36:41 +0300

    Add propTypes to component Display

---
+++ src/Display.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends React.Component {
+  render() {
+    return (
+      <div>
+  
+      </div>
+    )
+  }
+}
+
+Display.propTypes = {
+  value: PropTypes.string
+};
+
+Display.defaultProps = {
+  value: 0
+};
+
+export default Display;
