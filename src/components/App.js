e822549e08dcc0ae9998842b626b73bb1f3af3b2
kwizl <karis.njoroge@gmail.com>
2021-01-26 23:07:27 +0300

    Changed App component from stateless to stateful

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,28 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App(prop) {
-  const title = 'React Calculator';
-  return (
-    <>
-      <div className="title">
-        <h1>{ title }</h1>
-      </div>
-      <Display value={prop.value} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    };
+  }
+
+  render() {
+    const title = 'React Calculator';
+    return (
+      <>
+        <div className="title">
+          <h1>{ title }</h1>
+        </div>
+        <Display value={this.prop.value} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
