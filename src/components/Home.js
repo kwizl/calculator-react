07b1916874a5fc2d1a11288baa98aef5e34d0e12
kwizl <karis.njoroge@gmail.com>
2021-02-03 21:53:45 +0300

    Add React Router to project

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h3>Welcome to our page!</h3>
+    <p>
+      https://math-magiciansA Web PageMath MagiciansLorem ipsum dolor sit amet,
+      consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui o!cia deserunt
+      mollit anim id est laborum. IpsuLorem ipsum dolor sit amet, consectetur
+      adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim
+      id est laborum.
+    </p>
+  </div>
+);
+
+export default Home;
---
+++ src/components/Quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Quote = () => (
+  <h3>
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    it is about understanding. –William Paul Thurston
+  </h3>
+);
+
+export default Quote;
